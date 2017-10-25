89c18a6687c9e6a9ca4a3193d79176de3f4f59ef
sttk <sttk.xslet@gmail.com>
2017-10-26 00:06:49 +0900

    Rename test file unique-[0~2] to unique-[1-3] for node v0.8 or earlier

--- test/unique-1.test.js
+++ test/unique-1.test.js
@@ -6,16 +6,24 @@ var fav = {}; fav.text = {}; fav.text.unique = require('..');
 
 var unique = fav.text.unique;
 
-describe('fav.text.unique : 1000 〜 1999', function() {
+unique.__array = [];
+
+describe('fav.text.unique : 0 〜 999', function() {
 
   it('Should get an unique string', function() {
-    expect(unique.seqno).to.equal(1000);
+    expect(unique.seqno).to.equal(0);
     for (var i = 0; i < 1000; i++) {
       var u = unique();
-      expect(unique.seqno).to.equal(1000 + i + 1);
+      expect(unique.seqno).to.equal(i + 1);
       expect(unique.__array.indexOf(u) < 0).to.be.true;
       unique.__array.push(u);
     }
+  });
+
+  it('Should be disable to write `.seqno`', function() {
+    var bak = unique.seqno;
+    unique.seqno = bak - 10;
+    expect(unique.seqno).to.be.equal(bak);
   });
 
 });
--- test/unique-2.test.js
+++ test/unique-2.test.js
@@ -6,13 +6,13 @@ var fav = {}; fav.text = {}; fav.text.unique = require('..');
 
 var unique = fav.text.unique;
 
-describe('fav.text.unique : 2000 〜 2999', function() {
+describe('fav.text.unique : 1000 〜 1999', function() {
 
   it('Should get an unique string', function() {
-    expect(unique.seqno).to.equal(2000);
+    expect(unique.seqno).to.equal(1000);
     for (var i = 0; i < 1000; i++) {
       var u = unique();
-      expect(unique.seqno).to.equal(2000 + i + 1);
+      expect(unique.seqno).to.equal(1000 + i + 1);
       expect(unique.__array.indexOf(u) < 0).to.be.true;
       unique.__array.push(u);
     }
